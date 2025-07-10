5731f6d460a0b581777e2bf30acb163933ad6f6f
dqnyb <dqny2018@gmail.com>
2025-07-11 00:26:13 +0300

    last___

--- chatbot_LumeaTa/chatBot/chatbot.py
+++ chatbot_LumeaTa/chatBot/chatbot.py
@@ -479,21 +479,21 @@ def interests():
         if language_saved == "RO":
             log_message("USER", "vrea sa continuie cu rezervarea locului")
             reply = (
-                "Perfect! 😊 Pentru a continua cu rezervarea, te rog să-mi lași următoarele informații:\n"
-                "- Nume complet\n"
-                "- Număr de telefon\n"
-                "- Adresă de email\n"
-                "- Orice alte detalii relevante (ex: număr persoane, perioadă preferată)"
+                "Perfect! 😊 Pentru a continua cu rezervarea, te rog să-mi lași următoarele informații:<br>\n"
+                "- Nume complet <br>\n"
+                "- Număr de telefon (care se începe cu 0 sau +373) <br>\n"
+                "- Adresă de email <br>\n"
+                "- Orice alte detalii relevante (ex: număr persoane, perioadă preferată) <br>"
             )
         elif language_saved == "RU":
             log_message("ПОЛЬЗОВАТЕЛЬ", "хочет продолжить с бронированием места")
             reply = (
-                "Отлично! 😊 Чтобы продолжить бронирование, пожалуйста, укажите следующую информацию:\n"
-                "- Полное имя\n"
-                        "- Номер телефона\n"
-                        "- Адрес электронной почты\n"
-                        "- Любые дополнительные детали (например, количество людей, предпочитаемые даты)"
-                    )
+                "Отлично! 😊 Чтобы продолжить бронирование, пожалуйста, укажите следующую информацию:<br>\n"
+                "- Полное имя <br>\n"
+                "- Номер телефона (который начинается с 0 или +373)<br>\n"
+                "- Адрес электронной почты <br>\n"
+                "- Любые дополнительные детали (например, количество людей, предпочитаемые даты) <br>"
+            )
             
         ask_interests = {
             "full_message": reply
